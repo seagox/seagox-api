1d9d59c6c90b70cc8eef5bea596e14f0b4182393
jc_xzh <15160037796@163.com>
2023-02-03 14:52:41 +0800

    多余文件删除

--- src/main/java/com/seagull/oa/export/mapper/JellyExportDataMapper.java
+++
@@ -1,7 +0,0 @@
-package com.seagull.oa.export.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.seagull.oa.export.entity.JellyExportData;
-
-public interface JellyExportDataMapper extends BaseMapper<JellyExportData> {
-}
